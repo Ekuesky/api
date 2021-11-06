9fa1c55be63ea2326339fc4a6b5f8b9c6a0a396e
Ayiek Sky <ayiekue9127@gmail.com>
2021-11-06 10:25:10 +0000

    add model & route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ReviewController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::apiResources([
+//     'products' => ProductController::class,
+//     'reviews' => ReviewController::class,
+// ]);
+Route::apiResource('products', ProductController::class);
+Route::group(['prefix'=>'product'],function(){
+    Route::apiResource('/reviews', ReviewController::class)->except([
+        'create', 'store', 'update', 'destroy'
+    ]);
+});
+// Route::prefix('admin')->group(function () {
+//     Route::get('/users', function () {
+//         // Matches The "/admin/users" URL
+//     });
+// });
